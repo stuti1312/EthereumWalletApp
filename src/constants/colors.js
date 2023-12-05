c0193ca93da63d8f60b687b29237639bc1c4a1d1
stuti <stuti@magnetontech.com>
2023-12-05 12:39:42 +0530

    colors added

---
+++ src/constants/colors.js
@@ -0,0 +1,10 @@
+export default {
+  peach: '#ff959b',
+  bgColor: '#11121a',
+  yellow: '#f3b92c',
+  brown: '#53393b',
+  blue: '#1f88ff',
+  red: '#fa4154',
+  gray: '#727e8c',
+  offWhite: '#CFE7FF',
+};
